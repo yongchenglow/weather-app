0f2219c3d721abdc1278cb598290e9172b5c3057
Yong Cheng Low <lowyongcheng@hotmail.com>
2025-05-25 09:13:55 +0800

    chore: add prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import eslintPluginPrettier from "eslint-plugin-prettier";
+import prettier from "eslint-config-prettier";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +13,19 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  {
+    files: ["**/*.{js,ts,jsx,tsx}"],
+    plugins: {
+      prettier: eslintPluginPrettier,
+    },
+    rules: {
+      "prettier/prettier": "error", // ❗ Show Prettier formatting issues as ESLint errors
+    },
+  },
+
+  // 🛑 Disables ESLint rules that conflict with Prettier
+  prettier,
 ];
 
 export default eslintConfig;
