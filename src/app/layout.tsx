35c658317fc51281388979e90eb4e69c4b45d6ec
Yong Cheng Low <lowyongcheng@hotmail.com>
2025-05-27 09:52:24 +0800

    feat: adjust to use next font with noto sans

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ThemeRegistry from "../components/ThemeRegistry";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { notoSans } from "@/lib/font";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={notoSans.className}>
         <ThemeRegistry>{children}</ThemeRegistry>
       </body>
     </html>
