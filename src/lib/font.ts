783771c7920d70d2ba3bb516fdb63b59c11eda8e
Yong Cheng Low <lowyongcheng@hotmail.com>
2025-05-27 09:52:24 +0800

    feat: adjust to use next font with noto sans

---
+++ src/lib/font.ts
@@ -0,0 +1,8 @@
+import { Noto_Sans } from "next/font/google";
+
+const notoSans = Noto_Sans({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
+
+export { notoSans };
--- src/lib/theme.ts
+++ src/lib/theme.ts
@@ -1,6 +1,10 @@
+import { notoSans } from "@/lib/font";
 import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
+  typography: {
+    fontFamily: `"${notoSans.style.fontFamily}", "Roboto", "Arial", sans-serif`,
+  },
   palette: {
     primary: {
       main: "#1976d2",
