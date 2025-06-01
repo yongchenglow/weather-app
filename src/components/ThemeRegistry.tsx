c22d79d616c02054253dcd059969856ccc75448d
Yong Cheng Low <lowyongcheng@hotmail.com>
2025-06-01 16:48:23 +0800

    feat: add basic mui theme details for light and dark themes

--- src/components/ThemeRegistry.tsx
+++ src/components/ThemeRegistry.tsx
@@ -2,13 +2,13 @@
 
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ReactNode } from "react";
-import theme from "@lib/theme";
+import { lightTheme } from "@lib/theme";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
 
 export default function ThemeRegistry({ children }: { children: ReactNode }) {
   return (
     <AppRouterCacheProvider>
-      <ThemeProvider theme={theme}>
+      <ThemeProvider theme={lightTheme}>
         <CssBaseline />
         {children}
       </ThemeProvider>
