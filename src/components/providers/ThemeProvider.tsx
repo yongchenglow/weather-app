d0ebe7dffd272ff83dd4031b8a291236a25d26c5
Yong Cheng Low <lowyongcheng@hotmail.com>
2025-06-04 09:04:03 +0800

    feat: adjust background according to the system theme

--- src/components/providers/ThemeProvider.tsx
+++ src/components/providers/ThemeProvider.tsx
@@ -4,11 +4,13 @@ import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ReactNode } from "react";
 import createAppTheme from "@lib/theme";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
+import useSystemTheme from "@/hooks/useSystemTheme";
 
 export default function ThemeRegistry({ children }: { children: ReactNode }) {
+  const systemTheme = useSystemTheme();
   return (
     <AppRouterCacheProvider>
-      <ThemeProvider theme={createAppTheme()}>
+      <ThemeProvider theme={createAppTheme(systemTheme)}>
         <CssBaseline />
         {children}
       </ThemeProvider>
